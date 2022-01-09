c0fea4f22f34166541155cbfdd3a0b83407a5be6
AydenTech <haramchi77@gmail.com>
2022-01-09 18:47:25 +0900

    연습하면서 조금 수정이 됨

---
+++ 0109_juho_lesson.py
@@ -0,0 +1,2 @@
+import sys
+a =sdafa
---
+++ P1000.py
@@ -0,0 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
+naive_input = input()
+var1, var2 = naive_input.split()
+print(int(var1) + int(var2))
+
+
+# a, b = input().rstrip().split()
