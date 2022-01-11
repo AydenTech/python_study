a6e5afa579c7917202a14015c5d1ea918678f40f
AydenTech <haramchi77@gmail.com>
2022-01-12 00:08:37 +0900

    till_ex_2_2_2

--- King_2_2_if_elif_else.py
+++ King_2_2_if_elif_else.py
@@ -29,4 +29,30 @@ if x % y == 0:
 elif x % y != 0:
     print(f'{x} cannot be divided by {y}')
 
+## break : 조건문 종료
+max = 10
 
+while True:
+    num = int(input())
+    if num > max:
+        print(num, 'is too big!')
+        break
+
+## 연습문제
+number = int(input('input integer: '))
+if number == 1:
+    print('one')
+elif number == 2:
+    print('two')
+elif number ==3:
+    print('three')
+
+## 연습문제 input()으로 사용자로부터 입력받은 정수를 계속 더해나가다가, 음수가 입력되면 중단하고 그 전까지 계산한 값을 출력하는 파이썬 스크립트를 작성하세요.
+e = 0
+while True:
+    f = int(input('any integer: '))
+    if f <= 0:
+        break
+    else:
+        e = e + f
+        print(e)
