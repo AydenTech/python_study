4109d3ecaee0bf63c5242a0a8ec939a80c53107f
AydenTech <haramchi77@gmail.com>
2022-01-14 00:41:33 +0900

    예제 연습장

--- Prac_note.py
+++ Prac_note.py
@@ -44,12 +44,35 @@
 #     else:
 #         print(f'{year} is not a leap year')
 
-## for 문
-family = ['mother', 'father', 'gentleman', 'sexy lady']
-for x in family:        # family의 각 항목 x에 대하여:
-    print(x, len(x))    # x와 x의 길이를 출력하라.
+# ## for 문
+# family = ['mother', 'father', 'gentleman', 'sexy lady']
+# for x in family:        # family의 각 항목 x에 대하여:
+#     print(x, len(x))    # x와 x의 길이를 출력하라.
+#
+# a = [4, 5, 6, 7]
+# for i in a:
+#     if i <= 5:
+#         print(i)
 
-a = [4, 5, 6, 7]
-for i in a:
-    if i <= 5:
-        print(i)
+# for ab in range(3):
+#     print(3)
+
+# ## 예제) 정수 입력받아 1부터 그 수까지와 제곱수를 출력 for 활용
+# x = int(input('put any integer: '))
+# for i in range(1, x + 1):
+#     print(i, i * i)
+
+minmax = input('min max 를 입력: ').split() #.split() 을 이용하여 연속된 문자를 나눌 수 있음
+print(minmax)
+min = int(minmax[0])
+max = int(minmax[1])
+
+temp = int(input('temperature: '))
+
+while temp != -999:
+    if min <= temp <= max:
+        print('nothing to report')
+        temp = int(input('temperature: '))
+    else:
+        print('Alert!!!')
+        break
