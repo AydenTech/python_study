402bc6da31d838e45be4191ae315ae17b8bb503e
AydenTech <haramchi77@gmail.com>
2022-01-14 00:40:16 +0900

    for문까지 끝

--- King_2_3_for.py
+++ King_2_3_for.py
@@ -16,3 +16,27 @@ for j in b:
 c = int(input('정수를 입력하세요: '))
 for d in range(c):
     print('', c)
+
+# for ab in range(3):
+#     print(3)
+
+## 예제) 정수 입력받아 1부터 그 수까지와 제곱수를 출력 for 활용
+x = int(input('put any integer: '))
+for i in range(1, x + 1):
+    print(i, i * i)
+
+## 예제) 온도 범위 입력받고 그 외는 경고 및 종료, 범위 내면 계속, -999면 종료
+minmax = input('min max 를 입력: ').split() #.split() 을 이용하여 연속된 문자를 나눌 수 있음
+print(minmax)
+min = int(minmax[0])
+max = int(minmax[1])
+
+temp = int(input('temperature: '))
+
+while temp != -999:
+    if min <= temp <= max:
+        print('nothing to report')
+        temp = int(input('temperature: '))
+    else:
+        print('Alert!!!')
+        break
