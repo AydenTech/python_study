e168b424c3ea59bca59ddcb7cef3d9f0794505cf
AydenTech <haramchi77@gmail.com>
2022-01-16 19:09:01 +0900

    till return

--- King_3_Function.py
+++ King_3_Function.py
@@ -1,19 +1,19 @@
-# a = 3
-# for i in range(1, 10):
-#     print(a, '*', i, '=', a * i)
-#
-# b = 4
-# for i in range(1, 10):
-#     print(f'{b} * {i} = {b * i}')
-#
-# ##구구단 출력하기
-# def googoo(m):
-#     for i in range(1, 10):
-#         print(f'{m} * {i} = {m * i}')
-# m = int(input('integer: '))
-# googoo(m)
-#
-# ## 삼각형 넓이 구하기 return 활용 (함수)
+a = 3
+for i in range(1, 10):
+    print(a, '*', i, '=', a * i)
+
+b = 4
+for i in range(1, 10):
+    print(f'{b} * {i} = {b * i}')
+
+##구구단 출력하기
+def googoo(m):
+    for i in range(1, 10):
+        print(f'{m} * {i} = {m * i}')
+m = int(input('integer: '))
+googoo(m)
+
+## 삼각형 넓이 구하기 return 활용 (함수)
 
 width = int(input('밑변: '))
 height = int(input('높이: '))
@@ -29,3 +29,11 @@ def tri_area(width, height):
 
 ret = tri_area(width, height)
 print(ret)
+
+## 퀴즈 만들기
+def quiz():
+    ans = input('1 + 2 = ')
+    return 1 + 2 == int(ans)
+
+
+print(quiz())
