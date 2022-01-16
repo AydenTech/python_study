5e1a38d30a599a424a6d69de451fa8f329f0d987
AydenTech <haramchi77@gmail.com>
2022-01-16 19:02:01 +0900

    Chap3 함수

---
+++ King_3_Function.py
@@ -0,0 +1,31 @@
+# a = 3
+# for i in range(1, 10):
+#     print(a, '*', i, '=', a * i)
+#
+# b = 4
+# for i in range(1, 10):
+#     print(f'{b} * {i} = {b * i}')
+#
+# ##구구단 출력하기
+# def googoo(m):
+#     for i in range(1, 10):
+#         print(f'{m} * {i} = {m * i}')
+# m = int(input('integer: '))
+# googoo(m)
+#
+# ## 삼각형 넓이 구하기 return 활용 (함수)
+
+width = int(input('밑변: '))
+height = int(input('높이: '))
+
+print(f'밑변: {width}, 높이: {height}')
+
+
+def tri_area(width, height):
+    y = (1 / 2) * width * height
+    # print(y)
+    return y
+
+
+ret = tri_area(width, height)
+print(ret)
