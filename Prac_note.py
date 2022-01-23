ad0f5b68ab6c473b3368104bfb311cc2df92b6db
AydenTech <haramchi77@gmail.com>
2022-01-23 19:56:23 +0900

    Chap3까지의 Practice notation 업로드

--- Prac_note.py
+++ Prac_note.py
@@ -21,8 +21,8 @@
 #     print(f'{x}년은 윤년이 아닙니다')
 # else:
 #     print(f'{x}년은 윤년입니다')
-
-## 답안
+#
+# # 답안
 # while True:
 #     is_leap_year = None
 #
@@ -43,7 +43,7 @@
 #         print(f'{year} is a leap year')
 #     else:
 #         print(f'{year} is not a leap year')
-
+#
 # ## for 문
 # family = ['mother', 'father', 'gentleman', 'sexy lady']
 # for x in family:        # family의 각 항목 x에 대하여:
@@ -53,15 +53,15 @@
 # for i in a:
 #     if i <= 5:
 #         print(i)
-
+#
 # for ab in range(3):
 #     print(3)
-
+#
 # ## 예제) 정수 입력받아 1부터 그 수까지와 제곱수를 출력 for 활용
 # x = int(input('put any integer: '))
 # for i in range(1, x + 1):
 #     print(i, i * i)
-
+#
 # minmax = input('min max 를 입력: ').split() #.split() 을 이용하여 연속된 문자를 나눌 수 있음
 # print(minmax)
 # min = int(minmax[0])
@@ -76,8 +76,126 @@
 #     else:
 #         print('Alert!!!')
 #         break
+#
+# def printer_Lee():
+#     print('I am Haram Lee')
+#
+# printer_Lee()
+#
+# # 삼각형 넓이 구하기 return 활용 (함수)
+#
+# width = int(input('밑변: '))
+# height = int(input('높이: '))
+#
+# print(f'밑변: {width}, 높이: {height}')
+#
+#
+# def tri_area(width, height):
+#     y = (1 / 2) * width * height
+#     # print(y)
+#     return y
+#
+#
+# ret = tri_area(width, height)
+# print(ret)
+#
+# ## 퀴즈 만들기
+# def quiz():
+#     ans = input('1 + 2 = ')
+#     return 1 + 2 == int(ans)
+#
+#
+# print(quiz())
+#
+# # Korean_number 정의하기 함수
+#
+# def Korean_num(v):
+#     if v == 1:
+#        return '일'
+#     elif v == 2:
+#        return '이'
+#     elif v == 3:
+#         return '삼'
+#
+# a = int(input("num 1~3: "))
+# print(Korean_num(a))
+#
+# from datetime import datetime
+# today = datetime.today()
+# today
+# print(today)
+# print(today.year)
+# x = int(input('your birth year: '))
+#
+#
+# def age_fn(x):
+#     return today.year - x
+#
+#
+# print('your age is...')
+# print(age_fn(x))
+#
+# ## 단리 구하기 함수
+#
+#
+# def simple_interest(p, r, t):
+#     return p * r * t
+#
+#
+# p = float(input('원금: '))
+# r = float(input('이율: '))
+# t = float(input('시간: '))
+# print(simple_interest(p, r, t))
+#
+#
+# def simple_interest_amount(p, r, t):
+#     return p + p*r*t
+# p = float(input('원금: '))
+# r = float(input('이율: '))
+# t = float(input('시간: '))
+# print(simple_interest_amount(p, r, t))
 
-def printer_Lee():
-    print('I am Haram Lee')
+## 복리 원리금
+# def compound_interest_amount(p, r, t, n):
+#     return p*(1+r/n)**(n*t)
+# p = float(input('원금: '))
+# r = float(input('이율: '))
+# t = float(input('몇년간?: '))
+# n = float(input('연 몇번 적용?: '))
+# print(compound_interest_amount(p, r, t, n))
 
-printer_Lee()
+# ## 지역변수 전역변수
+# def e_is_10():
+#     global e  # 전역변수
+#     e = 10
+#     print('e 값은 ', e, '입니다')
+#     return e
+#
+# print(e_is_10())
+# print(e)
+
+# ## 매개변수
+# def hap(x, y):
+#     return x+y
+# print(hap(3, 4))
+
+# (lambda x,y: x+y)(10,20)
+# print((lambda x,y: x+y)(10,20))
+
+# map(lambda x: x ** 2, range(5))
+# list(map(lambda x: x ** 2, range(5)))
+
+## reduce(함수, 시퀀스)
+# from functools import reduce
+# reduce(lambda x, y: x + y, [0,1,2,3,4])
+# print(reduce(lambda x, y: x + y, [0,1,2,3,4]))
+#
+# from functools import reduce
+# reduce(lambda x, y: y+x, 'abcde')
+# print(reduce(lambda x, y: y+x, 'abcde'))
+
+## filter(lambda x: x<5, range(10))
+# print(list(filter(lambda x: x<5, range(10))))
+filter(lambda x: x % 2, range(10))
+list(filter(lambda x: x % 2, range(10)))
+print(list(filter(lambda x: x % 2, range(10))))
